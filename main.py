0e7f204ea83d2c389076831c0bd6f4ef60a4fece
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-11-10 09:15:01 +0000

    removed question and answering model, uninstalled unused libraries

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from exts import db
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
-from blueprints.question_and_answering.question_and_answering_route import (
-    question_and_answering_bp,
-)
+
 from blueprints.disease_prediction.disease_prediction_routes import (
     disease_prediction_bp,
 )
@@ -28,7 +26,6 @@ def create_app():
         Bcrypt(app)
         JWTManager(app)
         db.init_app(app)
-        app.register_blueprint(question_and_answering_bp, url_prefix="/api")
         app.register_blueprint(drug_recommendation_bp, url_prefix="/api")
         app.register_blueprint(disease_prediction_bp, url_prefix="/api")
 
--- run.py
+++ run.py
@@ -5,5 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     app = create_app()
-    print(f"VITAL AI SERVER RUNNING ON PORT 5001")
     app.run(debug=True, port=8005)
